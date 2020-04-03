a3c836acc2d730b2d7d0c57c3e78973e7211593a
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-03 22:17:19 +0300

    Add interaction for Button

--- RPS Lesson 20 ItADwS/Sign.swift
+++ RPS Lesson 20 ItADwS/Sign.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+import GameplayKit
+
+let randomChoice = GKRandomDistribution(lowestValue: 0, highestValue: 2)
+
+func randomSign() -> Sign {
+    let sign = randomChoice.nextInt()
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+
+enum Sign {
+    case rock, paper ,scissors
+    
+    var emoji : String {
+        switch self {
+        case .rock:
+            return "👊"
+        case .paper:
+            return "✋"
+        case .scissors:
+            return "✌️"
+        }
+    }
+    
+    func getResult(for opposite: Sign ) -> GameState {
+        switch (self, oposite) {
+        case (.rock, .rock),
+             (.paper, .paper),
+             (.scissors, .scissors):
+            return .draw
+        case (.rock, .scissors),
+             (.paper, .rock),
+             (.scissors, .paper):
+            return .win
+        default:
+            return .lose
+        }
+    }
+    
+    
+    
+    
+    //        switch self {
+    //        case .rock:
+    //            switch opposite {
+    //            case .rock:
+    //                return .draw
+    //            case .paper:
+    //                return .lose
+    //            case .scissors:
+    //                return .win
+    //            }
+    //        case .paper:
+    //            switch opposite {
+    //            case .rock:
+    //                return .win
+    //            case .paper:
+    //                return .draw
+    //            case .scissors:
+    //                return .lose
+    //            }
+    //        case .scissors:
+    //            switch opposite {
+    //            case .rock:
+    //                return .lose
+    //            case .paper:
+    //                return .win
+    //            case .scissors:
+    //                return .draw
+    //            }
+    //        }
+}
+
+
+
